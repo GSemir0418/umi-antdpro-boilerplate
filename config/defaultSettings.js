34d4a42ead35ffa5a9a13ba3cd307c5bead6e5d7
GSemir0418 <845217811@qq.com>
2021-06-01 10:34:51 +0800

    init commit

---
+++ config/defaultSettings.js
@@ -0,0 +1,15 @@
+const proSettings = {
+    navTheme: 'dark',
+    // 拂晓蓝
+    // primaryColor: '#1890ff',
+    primaryColor: '#6079B2',
+    layout: 'side',
+    contentWidth: 'Fluid',
+    fixedHeader: false,
+    fixSiderbar: true,
+    colorWeak: false,
+    title: '南京智能制造研究院',
+    pwa: false,
+    iconfontUrl: '',
+};
+export default proSettings;
---
+++ src/layouts/BasicLayout.jsx
@@ -0,0 +1,209 @@
+/**
+ * Ant Design Pro v4 use `@ant-design/pro-layout` to handle Layout.
+ *
+ * @see You can view component api by: https://github.com/ant-design/ant-design-pro-layout
+ */
+import ProLayout, { DefaultFooter, SettingDrawer } from '@ant-design/pro-layout';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { Link, useIntl, connect, history } from 'umi';
+import { GithubOutlined } from '@ant-design/icons';
+import { Result, Button, Tabs } from 'antd';
+import Authorized from '@/utils/Authorized';
+import RightContent from '@/components/GlobalHeader/RightContent';
+import { getMatchMenu } from '@umijs/route-utils';
+import styles from './BasicLayout.less'
+// import logo from '../assets/logo.svg';
+
+const tabMenus = []
+
+const noMatch = (
+  <Result
+    status={403}
+    title="403"
+    subTitle="Sorry, you are not authorized to access this page."
+    extra={
+      <Button type="primary">
+        <Link to="/user/login">Go Login</Link>
+      </Button>
+    }
+  />
+);
+
+/** Use Authorized check all menu item */
+const menuDataRender = (menuList) =>
+  menuList.map((item) => {
+
+    const tabMenu = tabMenus.filter(tabMenu => tabMenu.path === item.path)
+    if (tabMenu.length === 0 && item.name) {
+      tabMenus.push({
+        title: item.name,
+        key: item.path,
+        path: item.path
+      })
+    }
+
+    const localItem = {
+      ...item,
+      children: item.children ? menuDataRender(item.children) : undefined,
+    };
+    return Authorized.check(item.authority, localItem, null);
+  });
+
+// 页脚
+const defaultFooterDom = (
+  <DefaultFooter
+    copyright={`${new Date().getFullYear()} 创新产品开发部出品`}
+    links={[
+      {
+        key: 'SUPCON',
+        title: 'SUPCON',
+        href: 'https://www.supcontech.com/',
+        blankTarget: true,
+      },
+      {
+        key: 'github',
+        title: <GithubOutlined />,
+        href: 'https://github.com/GSemir0418/',
+        blankTarget: true,
+      },
+    ]}
+  />
+);
+
+const BasicLayout = (props) => {
+  const {
+    dispatch,
+    children,
+    settings,
+    location = {
+      pathname: '/',
+    },
+  } = props;
+
+  const [tabs, setTabs] = useState([])
+  const [activeTabKey, setActiveTabKey] = useState('')
+
+  const menuDataRef = useRef([]);
+  useEffect(() => {
+    if (dispatch) {
+      dispatch({
+        type: 'user/fetchCurrent',
+      });
+    }
+  }, []);
+  /** Init variables */
+
+  const handleMenuCollapse = (payload) => {
+    if (dispatch) {
+      dispatch({
+        type: 'global/changeLayoutCollapsed',
+        payload,
+      });
+    }
+  }; // get children authority
+
+  const authorized = useMemo(
+    () =>
+      getMatchMenu(location.pathname || '/', menuDataRef.current).pop() || {
+        authority: undefined,
+      },
+    [location.pathname],
+  );
+  const { formatMessage } = useIntl();
+  return (
+    <>
+      <ProLayout
+        style={styles}
+        logo={null}
+        formatMessage={formatMessage}
+        {...props}
+        {...settings}
+        onCollapse={handleMenuCollapse}
+        onMenuHeaderClick={() => history.push('/')}
+        menuItemRender={(menuItemProps, defaultDom) => {
+          if (
+            menuItemProps.isUrl ||
+            !menuItemProps.path ||
+            location.pathname === menuItemProps.path
+          ) {
+            return defaultDom;
+          }
+
+          return <Link to={menuItemProps.path}>{defaultDom}</Link>;
+        }}
+        breadcrumbRender={(routers = []) => [
+          {
+            path: '/',
+            breadcrumbName: formatMessage({
+              id: 'menu.home',
+            }),
+          },
+          ...routers,
+        ]}
+        itemRender={(route, params, routes, paths) => {
+          const first = routes.indexOf(route) === 0;
+          return first ? (
+            <Link to={paths.join('/')}>{route.breadcrumbName}</Link>
+          ) : (
+            <span>{route.breadcrumbName}</span>
+          );
+        }}
+        footerRender={() => {
+          if (settings.footerRender || settings.footerRender === undefined) {
+            return defaultFooterDom;
+          }
+
+          return null;
+        }}
+        menuDataRender={menuDataRender}
+        rightContentRender={() => <RightContent />}
+        onPageChange={() => {
+          const currentTab = tabs.filter(tab => tab.path === location.pathname)
+          if (currentTab.length === 0) {
+            const newTabs = [...tabs]
+            const currentTabMenu = tabMenus.filter(tabMenu => tabMenu.path === location.pathname)
+            if (currentTabMenu.length > 0) {
+              newTabs.push({
+                ...currentTabMenu[0]
+              })
+              setTabs(newTabs)
+            }
+          }
+          setActiveTabKey(location.pathname || '')
+        }}
+        postMenuData={(menuData) => {
+          menuDataRef.current = menuData || [];
+          return menuData || [];
+        }}
+      >
+        <Authorized authority={authorized.authority} noMatch={noMatch}>
+          <Tabs
+            type='editable-card'
+            activeKey={activeTabKey}
+            hideAdd={true}
+            defaultActiveKey='1'
+          >
+            {
+              tabs.map(tab => <Tabs.TabPane key={tab.key} tab={tab.title} />)
+            }
+          </Tabs>
+          {children}
+        </Authorized>
+      </ProLayout>
+      <SettingDrawer
+        settings={settings}
+        onSettingChange={(config) =>
+          dispatch({
+            type: 'settings/changeSetting',
+            payload: config,
+          })
+        }
+      />
+    </>
+  );
+};
+
+export default connect(({ global, settings }) => ({
+  collapsed: global.collapsed,
+  settings,
+}))(BasicLayout);
